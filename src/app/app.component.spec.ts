c571b7012cc6028beac1eb9198b4be48718d2be5
Sylvain Janet <sjanet@jehann.fr>
2022-12-09 03:27:34 +0100

    fixed test - CI/CD OK

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'personal-website-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-website-angularr');
+    expect(app.title).toEqual('personal-website-angular');
   });
 
   it('should render title', () => {
