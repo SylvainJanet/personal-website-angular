96fabf721d526e9a854d98c3cbad3bc5b91d3b9e
Sylvain Janet <sjanet@jehann.fr>
2022-12-11 11:03:26 +0100

    fixed ExpressionChangedAfterItHasBeenCheckedError

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,19 @@ import {
   PreloaderService,
   Preloaders,
 } from './services/preloader/preloader.service';
-import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
   loader = Preloaders.MAIN;
 
   constructor(
@@ -19,6 +24,9 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.preloader.hideLoader(this.loader);
+    this.cd.detectChanges();
+  }
+  ngAfterViewChecked() {
     this.cd.detectChanges();
   }
 }
