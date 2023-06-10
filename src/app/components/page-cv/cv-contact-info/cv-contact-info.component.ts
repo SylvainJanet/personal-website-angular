bd5d2a85e506c5a23eb5c8f1f9b851650d3ab98f
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-10 17:23:13 +0200

    Document components/page-cv/cv-contact-info

--- src/app/components/page-cv/cv-contact-info/cv-contact-info.component.ts
+++ src/app/components/page-cv/cv-contact-info/cv-contact-info.component.ts
@@ -4,19 +4,35 @@ import { ComponentWithText } from 'src/app/interfaces/ComponentWithText';
 import { TextService } from 'src/app/services/db/text/text.service';
 import { LanguageService } from 'src/app/services/language/language.service';
 
+/**
+ * Contact info component, containing basic contact info such as phone number,
+ * email and a quick review of my name and profile.
+ */
 @Component({
   selector: 'app-cv-contact-info',
   templateUrl: './cv-contact-info.component.html',
   styleUrls: ['./cv-contact-info.component.css'],
 })
 export class CvContactInfoComponent implements ComponentWithText, OnDestroy {
+  /** Text containing the name label. */
   name = of('');
+  /** Text containing the name. */
   sj = of('');
+  /** Text containing the profile label. */
   profile = of('');
+  /** Text containing the profile. */
   fsDev = of('');
+  /** Text containing the email label. */
   email = of('');
+  /** Text containingthe phone label. */
   phone = of('');
 
+  /**
+   * Contact info component constructor.
+   *
+   * @param languageService The {@link LanguageService}
+   * @param textService The {@link TextService}
+   */
   constructor(
     private languageService: LanguageService,
     private textService: TextService
@@ -25,6 +41,13 @@ export class CvContactInfoComponent implements ComponentWithText, OnDestroy {
     this.updateTexts();
   }
 
+  /**
+   * Update the component's texts when the language is updated. See
+   * {@link LanguageService}. The subscriber design pattern is used and this
+   * function is used when the service notifies its subscribers to update the
+   * text contents after a language change. Uses {@link TextService} to get those
+   * contents from the database.
+   */
   updateTexts(): void {
     this.name = this.textService.get('about-name-field');
     this.sj = this.textService.get('sylvain-janet');
@@ -33,6 +56,12 @@ export class CvContactInfoComponent implements ComponentWithText, OnDestroy {
     this.email = this.textService.get('about-email-field');
     this.phone = this.textService.get('about-phone-field');
   }
+
+  /**
+   * On destroy, the component has to be unsubscribed rom the
+   * {@link LanguageService} to avoid having the service try to notify a destroyed
+   * subscriber.
+   */
   ngOnDestroy(): void {
     this.languageService.unsubscribe(this);
   }
