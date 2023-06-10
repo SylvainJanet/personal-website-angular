d8f8a8b43878977fc1e943cc5644d39b50d8f2d9
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-10 16:55:52 +0200

    Document components/common/text-sub-paragraph

--- src/app/components/common/text-sub-paragraph/text-sub-paragraph.component.ts
+++ src/app/components/common/text-sub-paragraph/text-sub-paragraph.component.ts
@@ -2,12 +2,18 @@ import { Component, Input } from '@angular/core';
 import { SubParagraph } from '../../classes/SubParagraph';
 import { SubParagraphRoot } from 'src/app/enums/subParagraphRoot';
 
+/** Component used to display a {@link SubParagraph} entity. */
 @Component({
   selector: 'app-text-sub-paragraph',
   templateUrl: './text-sub-paragraph.component.html',
   styleUrls: [],
 })
 export class TextSubParagraphComponent {
+  /** The {@link SubParagraph} entity to display. */
   @Input() subPar: SubParagraph = new SubParagraph(SubParagraphRoot.BR, '');
+  /**
+   * The {@link SubParagraphRoot} enumeration, in a variable so that it can be
+   * used in html template.
+   */
   SubParagraphRoot = SubParagraphRoot;
 }
