4f88e82452f573b2eed61331bbd37713067b034c
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-10 16:51:52 +0200

    Document components/common/text-paragraph-set

--- src/app/components/common/text-paragraph-set/text-paragraph-set.component.ts
+++ src/app/components/common/text-paragraph-set/text-paragraph-set.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Paragraph } from '../../classes/Paragraph';
 
+/** Component used to display an array of {@link Paragraph} entities. */
 @Component({
   selector: 'app-text-paragraph-set',
   templateUrl: './text-paragraph-set.component.html',
   styleUrls: [],
 })
 export class TextParagraphSetComponent {
+  /** The array of {@link Paragraph} entities to display. */
   @Input() paragraphs: Paragraph[] = [];
 }
