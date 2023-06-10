2fce023df285eadc46b00794b76d9e7869111487
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-10 21:58:55 +0200

    Document components/page-cv/page-content

--- src/app/components/page-cv/page-content/page-content.component.ts
+++ src/app/components/page-cv/page-content/page-content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+/** Component displaying the entire CV page content. */
 @Component({
   selector: 'app-page-content',
   templateUrl: './page-content.component.html',
