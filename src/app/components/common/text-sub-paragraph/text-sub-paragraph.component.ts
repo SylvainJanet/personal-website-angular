d0dcb911f545ee1f9e027eba331411996ac8f143
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-11 15:32:52 +0200

    Complete the documentation of a component

--- src/app/components/common/text-sub-paragraph/text-sub-paragraph.component.ts
+++ src/app/components/common/text-sub-paragraph/text-sub-paragraph.component.ts
@@ -2,7 +2,15 @@ import { Component, Input } from '@angular/core';
 import { SubParagraph } from '../../classes/SubParagraph';
 import { SubParagraphRoot } from 'src/app/enums/subParagraphRoot';
 
-/** Component used to display a {@link SubParagraph} entity. */
+/**
+ * Component used to display a {@link SubParagraph} entity.
+ *
+ * Do note that those elements are designed with the existence of code injection
+ * in mind and/or malicious content in mind. For instance, `<a\>` links are
+ * designed to be always prefixed by the asset folder path and hence avoid any
+ * kind of attacks and injection of link to another domain (or even the same
+ * domain but with an unexpected path).
+ */
 @Component({
   selector: 'app-text-sub-paragraph',
   templateUrl: './text-sub-paragraph.component.html',
