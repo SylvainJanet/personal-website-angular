eadaa7abb58226b269ca2a76772fc3814e68a84a
Sylvain <sylvainjanet@wanadoo.fr>
2023-06-11 17:44:35 +0200

    Document interfaces

--- src/app/interfaces/ComponentWithText.ts
+++ src/app/interfaces/ComponentWithText.ts
@@ -1,5 +1,11 @@
 import { OnDestroy } from '@angular/core';
 
+/**
+ * Interface for components containing texts that are subject to language change
+ * update. See {@link LanguageService}. It extends OnDestroy so that the
+ * components unsubscribe before being destroyd and avoir having references to
+ * destroyed component in the service.
+ */
 export interface ComponentWithText extends OnDestroy {
   updateTexts(): void;
 }
