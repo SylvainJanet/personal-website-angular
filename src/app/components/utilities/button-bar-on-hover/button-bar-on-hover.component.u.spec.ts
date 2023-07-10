bc024286ece9841c98c198d1dd67de47c77a1b4b
Sylvain <sylvainjanet@wanadoo.fr>
2023-07-10 19:46:51 +0200

    Fix ButtonBarOnHover component test

--- src/app/components/utilities/button-bar-on-hover/button-bar-on-hover.component.u.spec.ts
+++ src/app/components/utilities/button-bar-on-hover/button-bar-on-hover.component.u.spec.ts
@@ -5,7 +5,7 @@ import { ButtonBarOnHoverComponent } from './button-bar-on-hover.component';
 import { EventEmitter } from '@angular/core';
 
 describe('LinkBarOnHoverComponent - unit', () => {
-  let linkBarOnHoverComponent: ButtonBarOnHoverComponent;
+  let buttonBarOnHoverComponent: ButtonBarOnHoverComponent;
   let logServiceGlobalSpy: jasmine.SpyObj<LogService>;
   let logServiceSpy: jasmine.SpyObj<LogService>;
   let DOMComputationServiceSpy: jasmine.SpyObj<DOMComputationService>;
@@ -35,29 +35,31 @@ describe('LinkBarOnHoverComponent - unit', () => {
         ],
       });
 
-      linkBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
+      buttonBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
     });
-    it('should created', () => {
-      expect(linkBarOnHoverComponent).toBeTruthy();
-      expect(linkBarOnHoverComponent.logger).toBe(logServiceSpy);
+    it('should create', () => {
+      expect(buttonBarOnHoverComponent).toBeTruthy();
+      expect(buttonBarOnHoverComponent.logger).toBe(logServiceSpy);
     });
 
     it('should set default values', () => {
-      expect(linkBarOnHoverComponent).toBeTruthy();
+      expect(buttonBarOnHoverComponent).toBeTruthy();
 
-      expect(linkBarOnHoverComponent.lineWidth).toBe('0%');
-      expect(linkBarOnHoverComponent.textColor).toBe('white');
-      expect(linkBarOnHoverComponent.lineColor).toBe('white');
-      expect(linkBarOnHoverComponent.press).toBeTruthy();
-      expect(linkBarOnHoverComponent.press).toBeInstanceOf(EventEmitter<Event>);
-      linkBarOnHoverComponent.text.subscribe({
+      expect(buttonBarOnHoverComponent.lineWidth).toBe('0%');
+      expect(buttonBarOnHoverComponent.textColor).toBe('white');
+      expect(buttonBarOnHoverComponent.lineColor).toBe('white');
+      expect(buttonBarOnHoverComponent.press).toBeTruthy();
+      expect(buttonBarOnHoverComponent.press).toBeInstanceOf(
+        EventEmitter<Event>
+      );
+      buttonBarOnHoverComponent.text.subscribe({
         next: (t) => {
           expect(t).toBe('Action');
         },
       });
-      expect(linkBarOnHoverComponent.buttonStyle).toBe('');
-      expect(linkBarOnHoverComponent.lineStyle).toBe('');
-      expect(linkBarOnHoverComponent.globalStyle).toBe('');
+      expect(buttonBarOnHoverComponent.buttonStyle).toBe('');
+      expect(buttonBarOnHoverComponent.lineStyle).toBe('');
+      expect(buttonBarOnHoverComponent.globalStyle).toBe('');
     });
 
     it('should set proper logger', () => {
@@ -68,7 +70,7 @@ describe('LinkBarOnHoverComponent - unit', () => {
     });
 
     it('should make the line disappear on creation', () => {
-      expect(linkBarOnHoverComponent.lineDisappears).toHaveBeenCalledTimes(1);
+      expect(buttonBarOnHoverComponent.lineDisappears).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -95,7 +97,7 @@ describe('LinkBarOnHoverComponent - unit', () => {
         ],
       });
 
-      linkBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
+      buttonBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
     });
     it('should change the lineWidth', () => {
       const eventSpy = jasmine.createSpyObj(Event, ['']);
@@ -103,9 +105,9 @@ describe('LinkBarOnHoverComponent - unit', () => {
       const expectedLength = (75 / 100) * length;
       DOMComputationServiceSpy.getActualWidth.and.returnValue(length);
 
-      linkBarOnHoverComponent.lineAppears(eventSpy);
+      buttonBarOnHoverComponent.lineAppears(eventSpy);
 
-      expect(linkBarOnHoverComponent.lineWidth).toBe(expectedLength + 'px');
+      expect(buttonBarOnHoverComponent.lineWidth).toBe(expectedLength + 'px');
     });
   });
 
@@ -132,18 +134,18 @@ describe('LinkBarOnHoverComponent - unit', () => {
         ],
       });
 
-      linkBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
+      buttonBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
     });
     it('should change the lineWidth', () => {
       const eventSpy = jasmine.createSpyObj(Event, ['']);
       const length = 200;
       DOMComputationServiceSpy.getActualWidth.and.returnValue(length);
 
-      linkBarOnHoverComponent.lineAppears(eventSpy);
+      buttonBarOnHoverComponent.lineAppears(eventSpy);
 
-      linkBarOnHoverComponent.lineDisappears();
+      buttonBarOnHoverComponent.lineDisappears();
 
-      expect(linkBarOnHoverComponent.lineWidth).toBe('0%');
+      expect(buttonBarOnHoverComponent.lineWidth).toBe('0%');
     });
   });
 
@@ -167,23 +169,23 @@ describe('LinkBarOnHoverComponent - unit', () => {
         ],
       });
 
-      linkBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
+      buttonBarOnHoverComponent = TestBed.inject(ButtonBarOnHoverComponent);
     });
     it('should call lineDisappears method', () => {
       const expectedEvent = new Event('click');
-      spyOn(linkBarOnHoverComponent, 'lineDisappears');
+      spyOn(buttonBarOnHoverComponent, 'lineDisappears');
 
-      linkBarOnHoverComponent.doAction(expectedEvent);
+      buttonBarOnHoverComponent.doAction(expectedEvent);
 
-      expect(linkBarOnHoverComponent.lineDisappears).toHaveBeenCalledTimes(1);
+      expect(buttonBarOnHoverComponent.lineDisappears).toHaveBeenCalledTimes(1);
     });
     it('should emit press event', () => {
       const pressSpy = jasmine.createSpyObj('EventEmitter', ['emit']);
-      linkBarOnHoverComponent.press = pressSpy;
+      buttonBarOnHoverComponent.press = pressSpy;
       const expectedEvent = new Event('click');
-      spyOn(linkBarOnHoverComponent, 'lineDisappears');
+      spyOn(buttonBarOnHoverComponent, 'lineDisappears');
 
-      linkBarOnHoverComponent.doAction(expectedEvent);
+      buttonBarOnHoverComponent.doAction(expectedEvent);
 
       expect(pressSpy.emit).toHaveBeenCalledOnceWith(expectedEvent);
     });
